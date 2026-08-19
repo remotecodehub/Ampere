5ea96ebeb97cecad3cdc4999f50702a38e43ad9a
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-08-19 04:07:39 -0300

    fix(mqtt): limit transaction to persistence

--- src/web/Ampere.Application/MQTT/Commands/StartBrokerCommand.cs
+++ src/web/Ampere.Application/MQTT/Commands/StartBrokerCommand.cs
@@ -1,8 +1,8 @@
-using Ampere.Application.Common.Abstractions;
 using Ampere.Application.MQTT.Responses;
+using Mediator;
 
 namespace Ampere.Application.MQTT.Commands;
 
 /// <summary>Starts the MQTT broker.</summary>
 public sealed record StartBrokerCommand
-    : ITransactionalRequest<BrokerStatusResponse>;
+    : IRequest<BrokerStatusResponse>;
